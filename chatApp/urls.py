bfd119550a9c62d8e5d38acb8a1e435750fbfedb
demo <niravgohel2000@gmail.com>
2024-04-02 13:07:43 +0530

    initial commit

---
+++ chatApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", index, name="index"),
+    path('<str:room>/', room, name='room'),
+    path('checkview', checkview, name='checkview'),
+    path('send', send, name='send'),
+    path('getMessages/<str:room>/', getMessages, name='getMessages'),
+]
